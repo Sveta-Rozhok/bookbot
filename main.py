f95dae90e22d7a51d00cd3678deb67cd231343d2
sveta_rozhok <smirnovalana.18.2000@mail.ru>
2024-11-05 21:04:01 +0400

    bookbot ready

--- main.py
+++ main.py
@@ -3,5 +3,32 @@ def main():
         file_contents = f.read()
         print(file_contents)
 
+        words = file_contents.split()
+        print(len(words))
+        char_dict = count_characters(file_contents)
+        print(char_dict)
+        sort_on(char_dict, words)
+        
+
+
+def count_characters(file_contents):
+    lowered_string = file_contents.lower()
+    dictionary = {}
+    for i in lowered_string:
+        if i in dictionary:
+            dictionary[i] += 1
+        else:
+            dictionary[i] = 1
+        
+    return dictionary
+
+
+def sort_on(dictionary, words):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(words)} words found in the document")
+    for key, value in dictionary.items():
+        print(f"The {key} character was found {value} times")
+
+
 main()
 
